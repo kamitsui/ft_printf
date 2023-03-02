524a9b6adea231d1d1b7ff89028853692e3de362
Kaishiro Mitsui <kamitsui@c1r16s5.42tokyo.jp>
2023-03-02 19:17:35 +0900

    Add tests for the following (check leaks, multiple call)

--- test/cmdline_arg.c
+++ test/cmdline_arg.c
@@ -1,12 +1,15 @@
 #include	<stdio.h>
+#include	<stdlib.h>
 //#include	"ft_printf.h"
 void	ft_printf(char *input, ...);
 
 int	main(int ac, char **av)
 {
-	if (ac >= 2)
+	if (ac > 2)
 		return (0);
-	printf("abc");
-	//ft_printf(av[1]);
+	//printf("abc");
+	ft_printf(av[1]);
+	ft_printf("\n");
+	system("leaks a.out");
 	return (0);
 }
---
+++ test/repeat_call.c
@@ -0,0 +1,11 @@
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	"ft_printf.h"
+
+int	main(void)
+{
+	ft_printf("12345");
+	ft_printf("67890");
+	//system("leaks a.out");
+	return (0);
+}
