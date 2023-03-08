7529bcb7b075cfbb3d360b5f1bc6c6196e7142fe
Kaishiro Mitsui <kamitsui@c1r16s1.42tokyo.jp>
2023-03-08 14:10:07 +0900

    Find problem of %c (null, dell)

--- src/conversion_utils/character.c
+++ src/conversion_utils/character.c
@@ -1,6 +1,10 @@
+#include <stdarg.h>
 #include "ft_printf.h"
+#include "process.h"
 
 void	character(t_state_machine *machine)
 {
-	machine += 0;
+	char	c;
+	c = (char)va_arg(*(machine->ap), int);
+	add_to_buff(c, machine);
 }
