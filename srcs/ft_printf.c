51dcb10647d1ea67e7c7f14c858f0f2b2ca86db0
kamitsui <kamitsui@student.42tokyo.jp>
2024-12-18 03:15:09 +0900

    Add ft_eprintf ( out to STDERR_FILENO )

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: kamitsui <kamitsui@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 14:49:27 by kamitsui          #+#    #+#             */
-/*   Updated: 2023/09/07 11:55:27 by kamitsui         ###   ########.fr       */
+/*   Updated: 2024/12/18 02:03:54 by kamitsui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,28 @@ int	ft_printf(const char *input, ...)
 		if (machine.out == NULL)
 			return (-1);
 		machine.out_size = write(1, machine.out, machine.out_size);
+	}
+	free(machine.out);
+	va_end(ap);
+	if (machine.state == ERROR)
+		return (-1);
+	return (machine.out_size);
+}
+
+int	ft_eprintf(const char *input, ...)
+{
+	t_sm	machine;
+	va_list	ap;
+
+	va_start(ap, input);
+	initialize_machine(&machine, &ap);
+	process(input, &machine);
+	if (machine.state != ERROR)
+	{
+		machine.out = join_to_out(machine.out, machine.buffer, machine.len);
+		if (machine.out == NULL)
+			return (-1);
+		machine.out_size = write(STDERR_FILENO, machine.out, machine.out_size);
 	}
 	free(machine.out);
 	va_end(ap);
